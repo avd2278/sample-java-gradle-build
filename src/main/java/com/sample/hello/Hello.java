843badf7733758203a5922ad17d8e12233f6e752
avd2278 <amanda.dickson@kyfb.com>
2025-07-03 14:15:20 -0400

    Create Hello.java

---
+++ src/main/java/com/sample/hello/Hello.java
@@ -0,0 +1,7 @@
+    package com.sample.hello;
+
+    public class App {
+        public static void main(String[] args) {
+            System.out.println("Hello from Gradle Java Application!");
+        }
+    }
